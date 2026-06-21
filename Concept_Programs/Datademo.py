363b96d14b72df3b1262b20aaf203715fe6ec74e
shambhurajewable <shambhurajewable2882@gmail.com>
2026-06-21 17:03:33 +0530

    Added new programs

---
+++ Concept_Programs/Datademo.py
@@ -0,0 +1,7 @@
+No = 11
+
+print ("value of No is : ",No)
+
+print("Id of No is",id(No))
+
+print("type of No is ", type(No))
---
+++ Concept_Programs/TypeDemo.py
@@ -0,0 +1,16 @@
+        #NUMBER
+no = 11           # int
+marks =  90.78    # float
+value = 8 + 5j    # complex
+
+print(type(no))
+print(type(marks))
+print(type(value))
+
+Gender = True 
+print (Gender)
+print(type(Gender))
+
+Demo = None
+print(Demo)
+print(type(Demo))
